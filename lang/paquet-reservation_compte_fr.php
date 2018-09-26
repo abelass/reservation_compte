98a4c9f5b3da81b09d922d18301be8b38a00e85c
abelass <rainer@websimple.be>
2018-09-26 10:50:37 +0200

    la fabrique

---
+++ lang/paquet-reservation_compte_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'reservation_compte_description' => 'Permettre aux utilisateurs de gérer/visuliser les données relatifs a leurs réservations',
+	'reservation_compte_nom' => 'Rérvation compte',
+	'reservation_compte_slogan' => 'Gestion publique des réservations',
+);
---
+++ reservation_compte_pipelines.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Utilisations de pipelines par Rérvation compte
+ *
+ * @plugin     Rérvation compte
+ * @copyright  2018
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Reservation_compte\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
